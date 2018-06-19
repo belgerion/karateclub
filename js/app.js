1fb93d688eeaa32bd3b6e76961d16904d1bae14b
Jeffrey Barnes <belgerion@gmail.com>
2018-06-19 15:05:25 -0600

    Changed structure for navigation

---
+++ js/app.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+	$('#whiteButton').click(function() {
+		$('#whiteReqs').toggleClass('collapse');
+	});
+	$('#whiteCollapse').click(function() {
+		$('#whiteReqs').addClass('collapse');
+	});
+	$('#yellowButton').click(function() {
+		$('#yellowReqs').toggleClass('collapse');
+	});
+	$('#yellowCollapse').click(function() {
+		$('#yellowReqs').toggleClass('collapse');
+	});
+	$('#orangeButton').click(function() {
+		$('#orangeReqs').toggleClass('collapse');
+	});
+	$('#orangeCollapse').click(function() {
+		$('#orangeReqs').toggleClass('collapse');
+	});
+	$('#greenButton').click(function() {
+		$('#greenReqs').toggleClass('collapse');
+	});
+	$('#greenCollapse').click(function() {
+		$('#greenReqs').toggleClass('collapse');
+	});
+	$('#blueButton').click(function() {
+		$('#blueReqs').toggleClass('collapse');
+	});
+	$('#blueCollapse').click(function() {
+		$('#blueReqs').toggleClass('collapse');
+	});
+	$('#purpleButton').click(function() {
+		$('#purpleReqs').toggleClass('collapse');
+	});
+	$('#purpleCollapse').click(function() {
+		$('#purpleReqs').toggleClass('collapse');
+	});
+	$('#brown3rdButton').click(function() {
+		$('#brown3rdReqs').toggleClass('collapse');
+	});
+	$('#brown3rdCollapse').click(function() {
+		$('#brown3rdReqs').toggleClass('collapse');
+	});
+	$('#brown2ndButton').click(function() {
+		$('#brown2ndReqs').toggleClass('collapse');
+	});
+	$('#brown2ndCollapse').click(function() {
+		$('#brown2ndReqs').toggleClass('collapse');
+	});
+	$('#brown1stButton').click(function() {
+		$('#brown1stReqs').toggleClass('collapse');
+	});
+	$('#brown1stCollapse').click(function() {
+		$('#brown1stReqs').toggleClass('collapse');
+	});
+	$('#black1stButton').click(function() {
+		$('#black1stReqs').toggleClass('collapse');
+	});
+	$('#black1stCollapse').click(function() {
+		$('#black1stReqs').toggleClass('collapse');
+	});
+	
+});
+
+/*   
+$('#whiteButton').onClick.toggleClass('collapse');
+
+<div class="col-md-1 btn-style position-left"><button class="whiteCon topButtons position-left">White Contrast</button></div>
+
+$('#blackxxButton').click(myFunction);
+	
+	function myFunction() {
+		var x = document.getElementById('blackxButton').value;
+		var btn = document.getElementById('blackxButton').innerHTML;
+		
+		if (btn === 'Show'){
+			document.getElementById(x + 'Button').innerHTML = "Hide";
+		}
+		else {
+			document.getElementById(x + 'Button').innerHTML = "Show";
+		}
+		
+		$('#' + x + 'Reqs').toggleClass('collapse');
+}
+
+*/
--- js/helper.js
+++
@@ -1,2 +0,0 @@
-//var KataName = "<ol><h4 class='name'>%data%</h4></ol>";
-//var KataSteps = "<li class='katasteps'>%data%</li>"
